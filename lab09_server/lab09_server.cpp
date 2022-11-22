773120d58b063562d0e36f09e13220bc4544d2fd
Ira Ivanishak <iraivanishakcont@gmail.com>
2022-11-23 00:46:16 +0200

    Full.

--- lab09_server/lab09_server.cpp
+++ lab09_server/lab09_server.cpp
@@ -7,30 +7,25 @@
 #include <strsafe.h>
 #include <sstream>
 
-
 #define PROHIBITED_AMOUNT 3
 #define BUFSIZE 512
 
 using namespace std;
 
 HANDLE hMutex = CreateMutex(0, FALSE, (LPCWSTR)"mutex");
- 
-int counter = 0;
-
 vector <string> prohibitedWords;
-
+int counter = 0;
 struct param {
     vector <string> words;
     string wordToCheck;
 };
-
 struct paramCl {
     HANDLE pipe;
     int id;
 };
 
 void updateColors(int id) {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), id+2);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), id + 2);
 }
 
 DWORD WINAPI MassageReview(__in LPVOID params) {
@@ -48,13 +43,11 @@ DWORD WINAPI MassageReview(__in LPVOID params) {
 
 DWORD WINAPI ClientThread(__in LPVOID lpvParam)
 {
-
     paramCl p = *(paramCl*)lpvParam;
     HANDLE hPipe = p.pipe;
     updateColors(p.id);
 
     cout << "ClientThread created" << "\n";
-    cout << "Reading user name..." << "\n";
 
     char buffer[BUFSIZE];
     DWORD bytesRead = 0;
@@ -66,188 +59,186 @@ DWORD WINAPI ClientThread(__in LPVOID lpvParam)
             NULL
     );
 
-     if (!result) {
-         cout << "Failed to read name from the pipe." << "\n";
-         CloseHandle(hPipe);
-         return 1;
-     }
-     buffer[bytesRead] = '\0';
-     string name(buffer);
-     ifstream usersFile;
-     usersFile.open("Users.txt");
-     string fileData;
-
-     updateColors(p.id);
-     cout << "Number of read bytes: " << bytesRead << "\n";
-     string bData = "Welcome";
-     int index = 0;
-     if (usersFile.is_open())
-     {
-         bool found = false;
-         string line;
-         getline(usersFile, line);
-         while (usersFile)
-         {
-             if (line == name) {
-                 bData += " back";
-                 found = true;
-                 break;
-             }
-             getline(usersFile, line);
-         }
-         usersFile.close();
-         if (!found) {
-             ofstream usersFile;
-             usersFile.open("Users.txt", ios_base::app);
-             usersFile << name + "\n";
-             usersFile.close();
-         }
-     }
-     bData += ", " + name;
-     cout << "Sending data to pipe..." << "\n";
-     DWORD bytesWritten = 0;
-     result = WriteFile(
-         hPipe,
-         bData.c_str(),
-         strlen(bData.c_str()),
-         &bytesWritten,
-         NULL
-     );
-
-     if (result)
-         cout << "Number of written bytes: " << bytesWritten << "\n";
-     else
-         cout << "Failed to send data." << "\n";
-     // ira -------------------------------------------
+    updateColors(p.id);
+    cout << "\n" << "Reading user name..." << "\n";
+    if (!result) {
+        cout << "Failed to read name from the pipe." << "\n";
+        CloseHandle(hPipe);
+        return 1;
+    }
+    buffer[bytesRead] = '\0';
+    string name(buffer);
+    ifstream usersFile;
+    usersFile.open("Users.txt");
+    string fileData;
 
 
+    cout << "Number of read bytes: " << bytesRead << "\n";
+    cout << "User: " << buffer << endl;
+
+    string bData = "Welcome";
+    int index = 0;
+    if (usersFile.is_open())
+    {
+        bool found = false;
+        string line;
+        getline(usersFile, line);
+        while (usersFile)
+        {
+            if (line == name) {
+                bData += " back";
+                found = true;
+                break;
+            }
+            getline(usersFile, line);
+        }
+        usersFile.close();
+        if (!found) {
+            ofstream usersFile;
+            usersFile.open("Users.txt", ios_base::app);
+            usersFile << name + "\n";
+            usersFile.close();
+        }
+    }
+    bData += ", " + name;
+    cout << "Sending data to pipe..." << "\n";
+    DWORD bytesWritten = 0;
+    result = WriteFile(
+        hPipe,
+        bData.c_str(),
+        strlen(bData.c_str()),
+        &bytesWritten,
+        NULL
+    );
+
+    if (result)
+        cout << "Number of written bytes: " << bytesWritten << "\n";
+    else
+        cout << "Failed to send data." << "\n";
+    // ira -------------------------------------------
    
-     // misha -------------------------------------------
-     ifstream wordsData;
-     string line;
-     string wordsStr = "";
-     wordsData.open("words.txt");
-     int wordsArrSize = 0;
+    // misha -----------------------------------------
+    ifstream wordsData;
+    string line;
+    string wordsStr = "";
+    wordsData.open("words.txt");
+    int wordsArrSize = 0;
 
-     if (wordsData.is_open())
-     {
-         getline(wordsData, line);
-         while (wordsData)
-         {
-             wordsStr += line + " ";
-             getline(wordsData, line);
-             wordsArrSize++;
-         }
-     }
-     wordsData.close();
-     line = "";
+    if (wordsData.is_open())
+    {
+        getline(wordsData, line);
+        while (wordsData)
+        {
+            wordsStr += line + " ";
+            getline(wordsData, line);
+            wordsArrSize++;
+        }
+    }
+    wordsData.close();
+    line = "";
 
-     int iter = 0;
-     stringstream ssin(wordsStr);
+    int iter = 0;
+    stringstream ssin(wordsStr);
 
-     string ssinBufStr;
-     while (ssin.good() && iter < wordsArrSize) {
-         ssin >> ssinBufStr;
-         prohibitedWords.push_back(ssinBufStr);
-         ssinBufStr = "";
-         ++iter;
-     }
+    string ssinBufStr;
+    while (ssin.good() && iter < wordsArrSize) {
+        ssin >> ssinBufStr;
+        prohibitedWords.push_back(ssinBufStr);
+        ssinBufStr = "";
+        ++iter;
+    }
 
-     iter = 0;
+    iter = 0;
 
-     cout << "Reading message from client..." << endl;
-     buffer[BUFSIZE];
-     bytesRead = 0;
-     result = ReadFile(
-         hPipe,
-         buffer,
-         BUFSIZE-1,
-         &bytesRead,
-         NULL
-     );
+    buffer[BUFSIZE];
+    bytesRead = 0;
+    result = ReadFile(
+        hPipe,
+        buffer,
+        BUFSIZE-1,
+        &bytesRead,
+        NULL
+    );
+    cout << "\n" << "Reading message from client..." << endl;
+    updateColors(p.id);
+    if (result) {
+        buffer[bytesRead] = '\0';
+        cout  << "Number of read bytes: " << bytesRead << endl;
+        cout << "Message: " << buffer << endl;
+    }
+    else {
+        cout << "Failed to read data from the pipe." << endl;
+    }
+    vector<string> words{};
 
-     updateColors(p.id);
-     if (result) {
-         buffer[bytesRead] = '\0';
-         cout << "Number of read bytes: " << bytesRead << endl;
-         cout << "Message: " << buffer << endl;
-     }
-     else {
-         cout << "Failed to read data from the pipe." << endl;
-     }
+    ssin.str(buffer);
+    wordsArrSize = 0;
 
+    for (int i = 0; i < bytesRead; i++)
+        if (buffer[i] == ' ')
+        {
+            ++wordsArrSize;
+        }
 
-     vector<string> words{};
-
-
-     ssin.str(buffer);
-     wordsArrSize = 0;
-
-     for (int i = 0; i < bytesRead; i++)
-         if (buffer[i] == ' ')
-         {
-             ++wordsArrSize;
-         }
-
-     while (ssin.good() && iter < wordsArrSize) {
-         ssin >> ssinBufStr;
-         words.push_back(ssinBufStr);
-         ssinBufStr = "";
-         ++iter;
-     }
+    while (ssin.good() && iter < wordsArrSize) {
+        ssin >> ssinBufStr;
+        words.push_back(ssinBufStr);
+        ssinBufStr = "";
+        ++iter;
+    }
 
 
 
-     HANDLE* h = new HANDLE[prohibitedWords.size()];
-     param* par = new param[prohibitedWords.size()];
-     int i = 0;
-     counter = 0;
-     for (string w : prohibitedWords) {
-         DWORD threadID;
-         par[i].words = words;
-         par[i].wordToCheck = w;
-         h[i] = CreateThread(
-             0,
-               0,  
-             (LPTHREAD_START_ROUTINE)MassageReview,
-             (LPVOID)&par[i++],
-             NULL,
-             &threadID);
-     }
-     WaitForMultipleObjects(prohibitedWords.size(), h, FALSE, INFINITE);
+    HANDLE* h = new HANDLE[prohibitedWords.size()];
+    param* par = new param[prohibitedWords.size()];
+    int i = 0;
+    counter = 0;
+    for (string w : prohibitedWords) {
+        DWORD threadID;
+        par[i].words = words;
+        par[i].wordToCheck = w;
+        h[i] = CreateThread(
+            0,
+              0,  
+            (LPTHREAD_START_ROUTINE)MassageReview,
+            (LPVOID)&par[i++],
+            NULL,
+            &threadID);
+    }
+    WaitForMultipleObjects(prohibitedWords.size(), h, FALSE, INFINITE);
 
 
-     cout << "Sending data to pipe..." << endl;
-     if (counter < PROHIBITED_AMOUNT)
-         bData = buffer;
-     else
-         bData = "Too many prohibited words in your message!";
+    cout << "Sending data to pipe..." << endl;
+    if (counter < PROHIBITED_AMOUNT)
+        bData = buffer;
+    else
+        bData = "Too many prohibited words in your message!";
 
-     bytesWritten = 0;
-     result = WriteFile(
-         hPipe,
-         bData.c_str(),
-         strlen(bData.c_str()),
-         &bytesWritten,
-         NULL
-     );
+    bytesWritten = 0;
+    result = WriteFile(
+        hPipe,
+        bData.c_str(),
+        strlen(bData.c_str()),
+        &bytesWritten,
+        NULL
+    );
 
-     if (result) {
-         cout << "Number of bytes sent: " << bytesWritten << endl;
-     }
-     else {
-         cout << "Failed to send data." << endl;
-     }
-     if (!DisconnectNamedPipe(hPipe))
-     {
-         cout << "Disconnect failed" << GetLastError() << endl;
-     }
-     else
-     {
-         cout << "Disconnect successful" << endl;
-     }
+    if (result) {
+        cout << "Number of bytes sent: " << bytesWritten << endl;
+    }
+    else {
+        cout << "Failed to send data." << endl;
+    }
+    if (!DisconnectNamedPipe(hPipe))
+    {
+        cout << "Disconnect failed" << GetLastError() << endl;
+    }
+    else
+    {
+        cout << "\nDisconnect successful" << endl;
+    }
 
-     // misha -------------------------------------------
+     // misha -----------------------------------------
 
      FlushFileBuffers(hPipe);
      DisconnectNamedPipe(hPipe);
@@ -258,22 +249,17 @@ DWORD WINAPI ClientThread(__in LPVOID lpvParam)
 }
 
 
-int main(int argc, const char** argv) {
-
-    int q = 2;
-
-    DWORD  dwThreadId = 0;
-    HANDLE  hThread = NULL;
+int main(int argc, const char** argv) 
+{
+    // ira -------------------------------------------  
+    DWORD dwThreadId = 0;
+    HANDLE hThread = NULL;
     HANDLE pipe;
-
-    cout << "Server started" << endl;
-
+    cout << "Server started" << "\n";
 
     int client = 0;
-    while(1)
+    while(true)
     {
-        // ira -------------------------------------------  
-
         pipe = CreateNamedPipe(
             L"\\\\.\\pipe\\my_pipe",
             PIPE_ACCESS_DUPLEX,
@@ -285,14 +271,13 @@ int main(int argc, const char** argv) {
             NULL
         );
 
-        BOOL result = pipe != NULL && pipe != INVALID_HANDLE_VALUE;
+        bool result = pipe != NULL && pipe != INVALID_HANDLE_VALUE;
         if (!result) {
             cout << "Failed in a pipe creating.";
             return 1;
         }
 
-        result = ConnectNamedPipe(pipe, NULL) ?
-            TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+        result = ConnectNamedPipe(pipe, NULL) ;
 
         if (result)
         {
@@ -300,7 +285,7 @@ int main(int argc, const char** argv) {
             par.pipe = pipe;
             par.id = client;
             updateColors(client);
-            cout<< "\n" <<"Client connected, creating a processing thread."<<"\n";
+            cout<< "\n" <<"Client connected."<<"\n";
             hThread = CreateThread(
                 NULL,              
                 0,                 
@@ -310,13 +295,13 @@ int main(int argc, const char** argv) {
                 &dwThreadId);    
 
             if (hThread == NULL)
-            {
                 return -1;
-            }
+
             else CloseHandle(hThread);
         }
         else
             CloseHandle(pipe);
+
         client++;
     }
 
